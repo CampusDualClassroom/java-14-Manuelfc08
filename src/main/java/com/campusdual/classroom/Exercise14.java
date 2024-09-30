8668ef24c2d74778041ec4804b2d9a33003c0439
Manuel Fernández Crego <mfernandezc22@fpcoruna.afundacion.org>
2024-09-30 11:53:37 +0200

    Ejer14 Completado

--- src/main/java/com/campusdual/classroom/Exercise14.java
+++ src/main/java/com/campusdual/classroom/Exercise14.java
@@ -1,12 +1,22 @@
 package com.campusdual.classroom;
 
 public class Exercise14 {
-
     public static void main(String[] args) {
         Person p = new Person("John", "Smith");
         Teacher t = new Teacher("Maria", "Montessori", "Educación");
         PoliceOfficer po = new PoliceOfficer("Jake", "Peralta", "B-99");
         Doctor d = new Doctor("Gregory", "House", "Nefrología e Infectología");
-    }
 
+        System.out.println("Detalles de Person:");
+        p.getDetails();
+
+        System.out.println("\nDetalles de Teacher:");
+        t.getDetails();
+
+        System.out.println("\nDetalles de PoliceOfficer:");
+        po.getDetails();
+
+        System.out.println("\nDetalles de Doctor:");
+        d.getDetails();
+    }
 }
